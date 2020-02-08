5eb96367e9f16c539d67273a5a628c5e1da72de4
zebus3d <zebus3dream@gmail.com>
2020-02-08 16:55:56 +0100

    nada importante

--- nodes/object_operators/voronoi/cython/src/setup.py
+++ nodes/object_operators/voronoi/cython/src/setup.py
@@ -3,12 +3,15 @@ from Cython.Build import cythonize
 import numpy
 
 # For compile this:
+# cd nodes/object_operators/voronoi/cython/src/
 # python3 setup.py build_ext --inplace
+# mv -i voronoi.cpython-37m-x86_64-linux-gnu.so ../voronoi.cpython-37m-x86_64-linux-gnu.so
 
 # Or if you dont like create this setup.py:
 # you can compile with:
 # sudo pip3 install easycython
 # easycython voronoi.pyx
+# mv -i voronoi.cpython-37m-x86_64-linux-gnu.so ../voronoi.cpython-37m-x86_64-linux-gnu.so
 
 setup(
     name = 'voronoi',
